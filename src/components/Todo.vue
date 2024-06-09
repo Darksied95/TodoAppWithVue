9a19747eaf8e826bf7b17083c8aa87b2c117fd33
Aliyu Efe <aliyuefe9501@gmail.com>
2024-06-09 15:27:05 +0100

    update todo list after creation

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -5,6 +5,12 @@
         class=" flex items-center  gap-3 bg-d-VeryDarkDesaturatedBlue text-d-LightGrayishBlue h-10 border-b border-b-d-VeryDarkGrayishBlue-50">
         <span
             class="ml-4 inline-block w-4 aspect-square border border-solid border-d-VeryDarkGrayishBlue rounded-[100%]"></span>
-        <p class="text-sm">Complete online Javascript course</p>
+        <p class="text-sm">{{ todo.message }}</p>
     </li>
 </template>
+
+
+<script setup lang="ts">
+import type { ITodoList } from '@/types';
+defineProps<{ todo: ITodoList }>()
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import Todo from './Todo.vue';
+import type { ITodoList } from '@/types';
+
+const { todoListState } = defineProps<{ todoListState: ITodoList[] }>()
+
+
 </script>
 
 <template>
     <ul>
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
+        <Todo v-for="todo in todoListState" :todo :key="todo.id" />
     </ul>
 </template>
